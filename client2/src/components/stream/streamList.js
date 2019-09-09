b30bc359d42301a1664281e0871e67a1f21cb7a0
sintaro0221 <dorichan0629@gmail.com>
2019-09-09 12:08:34 +0900

    Stream redux -Bulk action creator

--- client2/src/components/stream/streamList.js
+++ client2/src/components/stream/streamList.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
+class streamList extends React.Component{
+	componentDidMount(){
+		this.props.fetchStreams();
+	}
 
-const streamList = () =>{
-	return (
+	renderList(){
+		return this.props.streams.map(stream =>{
+			return (
+				<div className="item" key={stream.id}>
+					<i className="large middle aligned icon camera" />
+					<div className="content">
+						{stream.title}
+						<div className="description">{stream.description}</div>
+					</div>
+				</div>
+				)
+		})
+	}
+
+	render() {
+		return (
 		<div>
-			streamList 
+			<h2>StreamList</h2>
+			<div className="ui called list">{this.renderList()}</div>
 		</div>
-	)
+		)}
+}
+const mapStateToProps = (state) =>{
+	return { streams:Object.values(state.streams)};
 }
 
-export default streamList
+export default connect(
+	mapStateToProps,
+	{ fetchStreams }
+)(streamList);
---
+++ client2/src/reducers/streamReducer.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import {
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state={},action) =>  {
+    switch (action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload,'id')};
+        case FETCH_STREAM:
+            return { ...state,[action.payload.id]: action.payload};
+        case CREATE_STREAM:
+            return { ...state,[action.payload.id]:action.payload};
+        case EDIT_STREAM:
+            return { ...state,[action.payload.id]:action.payload};
+        default:
+            return state;
+    }
+}
